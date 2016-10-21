4dbe584dc41a511489d5e46d0da1fc6b19591861
Richard Hagen <richard.hagen@gmail.com>
2016-10-21 09:37:48 +0700

    updated header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularFire} from "angularfire2";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(af: AngularFire) {}
 }
