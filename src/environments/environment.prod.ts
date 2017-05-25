976d27f59a87e3a5e7f860a6900fcf11d1fe80d5
Richard Hagen <richard.hagen@gmail.com>
2017-05-25 15:15:29 +0200

    Upgrade Releaser to Angular 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: "releaseer-a6183",
+    apiKey: "AIzaSyA_tWprch4_iJKKcEg3CCAO9VwCjvKxY5I",
+    authDomain: "releaseer-a6183.firebaseapp.com",
+    databaseURL: "https://releaseer-a6183.firebaseio.com",
+    storageBucket: "releaseer-a6183.appspot.com",
+    messagingSenderId: "454541533417"
+  }
 };
